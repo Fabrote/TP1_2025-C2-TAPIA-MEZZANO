39eff791087ec143134f21d6f23b1527956258b9
Fabrizio Tapia <fabriziojt1@gmail.com>
2025-10-16 23:36:47 -0300

    commit con validaciones

---
+++ UNIDAD 1/PDI_U1_ej1_Visualizacion.py
@@ -0,0 +1,25 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+img1 = cv2.imread(r'D:\FABRO\TUIA\PROCESAMIENTO DE IMAGENES\UNIDAD 1\xray-chest.png',cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread(r'D:\FABRO\TUIA\PROCESAMIENTO DE IMAGENES\UNIDAD 1\cameraman.tif',cv2.IMREAD_GRAYSCALE)
+
+# --- Matplotlib -------------------------------------------------
+plt.figure(1)
+h = plt.imshow(img1, cmap='gray')
+plt.colorbar(h)
+#plt.show()
+
+plt.figure(2)
+h = plt.imshow(img2, cmap='gray', vmin=0, vmax=50)
+plt.colorbar(h)
+
+#plt.show()
+plt.show(block=False) 
+
+# --- Open CV -----------------------------------------------------
+cv2.imshow('Imagen 1', img1)
+cv2.imshow('Imagen 2', img2)
+cv2.waitKey(0)
+#cv2.destroyAllWindows()
